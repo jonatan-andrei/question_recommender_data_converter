4b76b3eefe3fa8ec0b3c0a44e6798f187e6ff155
jonatan.andrei <jonatan.andrei.haas@gmail.com>
2022-10-14 19:24:14 -0300

    Código de teste

--- src/main/java/jonatan/andrei/service/ReadXmlFileService.java
+++ src/main/java/jonatan/andrei/service/ReadXmlFileService.java
@@ -27,6 +27,13 @@ public class ReadXmlFileService {
     public List<Map<String, String>> readXmlFile(String dumpName, String fileName, Class T) {
         List<Map<String, String>> mapFieldsList = new ArrayList<>();
         try {
+            File testFile = new File(File.separator + "opt" + File.separator + "dumps" + File.separator + dumpName + File.separator + "jonatanteste.txt");
+            if (testFile.createNewFile()) {
+                System.out.println("File created: " + testFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
             // Reference: https://mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
