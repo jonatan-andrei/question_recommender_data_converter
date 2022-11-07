e5c9093a7da77edd3478e8cd8d61526cdc9703b7
jonatan.andrei <jonatan.andrei.haas@gmail.com>
2022-11-07 01:26:41 -0300

    Adicionado endpoint para realizar todos os testes

--- src/main/java/jonatan/andrei/resource/AutomatedTestsResource.java
+++ src/main/java/jonatan/andrei/resource/AutomatedTestsResource.java
@@ -9,6 +9,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Path("/automated-tests")
 public class AutomatedTestsResource {
@@ -27,6 +30,16 @@ public class AutomatedTestsResource {
     @Produces(MediaType.TEXT_PLAIN)
     public void startAutomatedTests(@QueryParam("testInformation") String testInformation, @QueryParam("settings") SettingsModelType settings, @QueryParam("clearQR") boolean clearQR, @QueryParam("clearQRDatabase") boolean clearQRDatabase) {
         automatedTestsService.startTestByTestInformation(testInformation, settings, clearQR, clearQRDatabase);
+    }
+
+    @POST
+    @Path("/test-information/all")
+    @Produces(MediaType.TEXT_PLAIN)
+    public void startAllAutomatedTests(@QueryParam("testInformation") String testInformation, @QueryParam("clearQR") boolean clearQR, @QueryParam("clearQRDatabase") boolean clearQRDatabase) {
+        List<SettingsModelType> settingsList = asList(SettingsModelType.values());
+        for (SettingsModelType settings : settingsList) {
+            automatedTestsService.startTestByTestInformation(testInformation, settings, clearQR, clearQRDatabase);
+        }
     }
 
 }
